20881e72da329d66857d63b9c0bf2a9aa1ce6076
omar reda <omaraladeeb45@gmail.com>
2025-05-16 23:07:43 +0300

    Add project files.

---
+++ ConsoleApp1.Tests/Test1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApp1.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
